bd208a10d08b94818f6dbee8153f2b4e8323e2e3
nobudgetsec <nobudgetsec@users.noreply.github.com>
2025-05-21 17:31:22 +0000

    deploy: ce47a466167976602c69f1f30555ab89cc8b6573

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Welcome to No Budget Security",
+        "excerpt":"Welcome to No Budget Security Hello and welcome to my blog! This is a space where I’ll be sharing my thoughts, experiences, and knowledge about information security and technology. What to Expect In this blog, you’ll find: Security best practices Technology insights Real-world experiences Tips and tricks Why “No Budget...","categories": ["Blog"],
+        "tags": ["welcome","cybersecurity"],
+        "url": "/blog/2024/03/20/welcome-to-no-budget-security.html",
+        "teaser": null
+      }]
